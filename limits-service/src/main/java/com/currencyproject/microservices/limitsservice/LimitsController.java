c6e5259ba61ddc9f334632ff021874731b18473e
nisarg <patil_nisarg@yahoo.com>
2018-10-03 02:55:33 +0530

    connecting limits-service to config server

--- limits-service/src/main/java/com/currencyproject/microservices/limitsservice/LimitsController.java
+++ limits-service/src/main/java/com/currencyproject/microservices/limitsservice/LimitsController.java
@@ -1,27 +1,16 @@
 package com.currencyproject.microservices.limitsservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 	
-	@Value("${congfig.value.minimum}")
-	private int minimum;
-	@Value("${congfig.value.miximum}")
-	private int maximum;
-	
 	@Autowired
 	private ValueConfiguration config;
 	
-	@GetMapping("/limits-from-properties")
-	public LimitConfig rerieveLimitsFromPropertiesFile() {
-		return new LimitConfig(minimum, maximum);
-	}
-	
-	@GetMapping("/limits-from-config-class")
+	@GetMapping("/limits")
 	public LimitConfig rerieveLimitsFromConfigClass() {
 		return new LimitConfig(config.getMinimum(), config.getMaximum());
 	}
